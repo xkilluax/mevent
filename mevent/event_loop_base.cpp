e713f793798b1fafed919250c014c1b45003139e
looyao <looyao@thinksky.hk>
2017-02-09 19:47:52 +0800

    delete repeat files

--- mevent/event_loop_base.cpp
+++
@@ -1,5 +0,0 @@
-#if defined(__APPLE__) || defined(__FreeBSD__)
-#include "event_loop_base_kqueue.cpp"
-#elif defined(__linux__)
-#include "event_loop_base_epoll.cpp"
-#endif
--- mevent/mevent.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MEVENT_H
-#define _MEVENT_H
-
-#define CRLF        "\r\n"
-
-#define SERVER      "mevent"
-
-#endif
